8b0c89d6cb485c3ca7ee39bacf8a835a3ed76809
Alanistm20 <alanis.2016030855@gmail.com>
2025-06-26 00:31:14 -0500

    subiendo cambios recientes al proyecto

--- src/main/java/com/cibertec/pos_system/controller/ProductoWebController.java
+++ src/main/java/com/cibertec/pos_system/controller/ProductoWebController.java
@@ -1,10 +1,13 @@
 package com.cibertec.pos_system.controller;
 
+import com.cibertec.pos_system.entity.ProductoEntity;
 import com.cibertec.pos_system.service.ProductoService;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+
+import java.util.List;
 
 @Controller
 @RequestMapping("/producto")
@@ -19,6 +22,14 @@ public class ProductoWebController {
     @GetMapping
     public String listarProductos(Model model) {
         model.addAttribute("productos", productoService.listar());
-        return "producto/lista"; // Asegúrate de tener templates/producto/lista.html
+        return "producto/lista";
+    }
+
+    // ✅ Nuevo método para mostrar productos con descuento
+    @GetMapping("/descuentos")
+    public String listarConDescuento(Model model) {
+        List<ProductoEntity> productosConDescuento = productoService.listarSoloConDescuento();
+        model.addAttribute("productos", productosConDescuento);
+        return "descuento/preciosConDescuento"; // tu vista ya creada
     }
 }
